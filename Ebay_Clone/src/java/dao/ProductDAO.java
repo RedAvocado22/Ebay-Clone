a78f9b82c9f730378f4d8eba4ce8548a43e109df
Zero <nguyenhoangviet531@gmail.com>
2024-10-13 23:01:47 +0700

    feat: Implement ProductDAO class and CategoryDAO class for product management
    - Added methods to retrieve all products, search by name, and find by ID.
    - Added pagination support for retrieving products by page.
    - Included methods for counting total records and filtering products by category.
    - Ensured that SQL queries are structured to prevent SQL injection by using prepared statements.

--- Ebay_Clone/src/java/dao/ProductDAO.java
+++ Ebay_Clone/src/java/dao/ProductDAO.java
@@ -4,7 +4,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import models.Account;
+import models.Categories;
 import models.Product;
 import utils.DBUtils;
 
@@ -69,6 +72,35 @@ public class ProductDAO extends DBUtils {
             System.err.println(e.getMessage());
         }
         return listFound;
+    }
+
+    public Product findById(Product p) {
+        Product productFound = null;
+        con = getConnection();
+        String sql = "SELECT p.*\n"
+                + "FROM [dbo].[Product] p\n"
+                + "JOIN [dbo].[Account] a\n"
+                + "ON p.Seller = a.Username\n"
+                + "WHERE [ID] = ? ";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setObject(1, p.getId());
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String name = rs.getString("Name");
+                double price = rs.getDouble("Price");
+                int quantity = rs.getInt("Quantity");
+                String img = rs.getString("Image");
+                String seller = rs.getString("Seller");
+                Account sellerAccount = new Account();
+                sellerAccount.setUsername(seller);
+                productFound = new Product(id, name, price, quantity, img, sellerAccount);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return productFound;
     }
 
     public void insertNewProduct(Product product) {
@@ -168,6 +200,186 @@ public class ProductDAO extends DBUtils {
             }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
+        }
+        return listFound;
+    }
+
+    public int findTotalRecord() {
+        int totalRecord = 0;
+        con = getConnection();
+        String sql = "SELECT COUNT(*) AS total\n"
+                + "  FROM [dbo].[Product]\n";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                totalRecord = rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return totalRecord;
+    }
+
+    public List<Product> findByPage(int page) {
+        List<Product> listFound = new ArrayList<>();
+        con = getConnection();
+        int pageSize = 30;
+        int offset = (page - 1) * pageSize;
+        String sql = "SELECT *\n"
+                + "FROM [dbo].[Product]\n"
+                + "ORDER BY [ID]\n"
+                + "OFFSET ? ROWS\n"
+                + "FETCH NEXT ? ROWS ONLY";
+        try {
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1, offset);
+            ps.setInt(2, pageSize);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String name = rs.getString("Name");
+                double price = rs.getDouble("Price");
+                int quantity = rs.getInt("Quantity");
+                String img = rs.getString("Image");
+                String seller = rs.getString("Seller");
+                Account sellerAccount = new Account();
+                sellerAccount.setUsername(seller);
+                Product product = new Product(id, name, price, quantity, img, sellerAccount);
+                listFound.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listFound;
+    }
+
+    public int findTotalRecordByCategory(Categories category) {
+        int totalRecord = 0;
+        con = getConnection();
+        String sql = "SELECT COUNT(p.ID ) as total\n"
+                + "FROM [dbo].[Product] p\n"
+                + "RIGHT OUTER JOIN [dbo].[Products_To_Categories] ptc \n"
+                + "ON p.ID = ptc.ID_Product\n"
+                + "RIGHT OUTER JOIN [dbo].[Categories] c\n"
+                + "ON c.[ID] = ptc.[ID_Categories]\n"
+                + "WHERE c.Name = ? ";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setObject(1, category.getName());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                totalRecord = rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return totalRecord;
+    }
+
+    public List<Product> findByCategory(Categories category, int page) {
+        List<Product> listFound = new ArrayList<>();
+        con = getConnection();
+        int pageSize = 30;
+        int offset = (page - 1) * pageSize;
+        String sql = "SELECT p.*\n"
+                + "FROM [dbo].[Product] p\n"
+                + "RIGHT OUTER JOIN [dbo].[Products_To_Categories] ptc \n"
+                + "ON p.ID = ptc.ID_Product\n"
+                + "RIGHT OUTER JOIN [dbo].[Categories] c\n"
+                + "ON c.[ID] = ptc.[ID_Categories]\n"
+                + "WHERE c.Name = ? \n"
+                + "ORDER BY p.ID\n"
+                + "OFFSET ? ROWS\n"
+                + "FETCH NEXT ? ROWS ONLY;";
+        try {
+            ps = con.prepareStatement(sql);
+
+            ps.setObject(1, category.getName());
+            ps.setInt(2, offset);
+            ps.setInt(3, pageSize);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String name = rs.getString("Name");
+                double price = rs.getDouble("Price");
+                int quantity = rs.getInt("Quantity");
+                String img = rs.getString("Image");
+                String seller = rs.getString("Seller");
+                Account sellerAccount = new Account();
+                sellerAccount.setUsername(seller);
+                Product product = new Product(id, name, price, quantity, img, sellerAccount);
+                listFound.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listFound;
+    }
+
+    public int findTotalRecordByName(String keyword) {
+        int totalRecord = 0;
+        con = getConnection();
+        String sql = "SELECT COUNT(p.ID ) as total\n"
+                + "FROM [dbo].[Product] p\n"
+                + "RIGHT OUTER JOIN [dbo].[Products_To_Categories] ptc \n"
+                + "ON p.ID = ptc.ID_Product\n"
+                + "RIGHT OUTER JOIN [dbo].[Categories] c\n"
+                + "ON c.[ID] = ptc.[ID_Categories]\n"
+                + "WHERE p.Name LIKE ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setObject(1, "%" + keyword + "%");
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                totalRecord = rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return totalRecord;
+    }
+
+    public List<Product> findByName(String keyword, int page) {
+        List<Product> listFound = new ArrayList<>();
+        con = getConnection();
+        int pageSize = 30;
+        int offset = (page - 1) * pageSize;
+        String sql = "SELECT p.*\n"
+                + "FROM [dbo].[Product] p\n"
+                + "RIGHT OUTER JOIN [dbo].[Products_To_Categories] ptc \n"
+                + "ON p.ID = ptc.ID_Product\n"
+                + "RIGHT OUTER JOIN [dbo].[Categories] c\n"
+                + "ON c.[ID] = ptc.[ID_Categories]\n"
+                + "WHERE p.Name LIKE ? \n"
+                + "ORDER BY p.ID\n"
+                + "OFFSET ? ROWS\n"
+                + "FETCH NEXT ? ROWS ONLY;";
+        try {
+            ps = con.prepareStatement(sql);
+
+            ps.setObject(1, "%" + keyword + "%");
+            ps.setInt(2, offset);
+            ps.setInt(3, pageSize);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String name = rs.getString("Name");
+                double price = rs.getDouble("Price");
+                int quantity = rs.getInt("Quantity");
+                String img = rs.getString("Image");
+                String seller = rs.getString("Seller");
+                Account sellerAccount = new Account();
+                sellerAccount.setUsername(seller);
+                Product product = new Product(id, name, price, quantity, img, sellerAccount);
+                listFound.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return listFound;
     }
