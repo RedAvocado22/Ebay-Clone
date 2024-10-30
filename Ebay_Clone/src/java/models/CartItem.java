9526e826115a88e235c54f1fd4a0825dfa0903ab
RedAvocado <minhcuong2922004@gmail.com>
2024-10-30 16:29:47 +0700

    feat: update product and cart item
    Signed-off-by: RedAvocado <minhcuong2922004@gmail.com>

--- Ebay_Clone/src/java/models/CartItem.java
+++ Ebay_Clone/src/java/models/CartItem.java
@@ -5,6 +5,14 @@ public class CartItem {
     private Product product;
     private int quantity;
 
+    public CartItem() {
+    }
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
     public Product getProduct() {
         return product;
     }
