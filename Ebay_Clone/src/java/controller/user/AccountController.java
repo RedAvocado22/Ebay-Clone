8e40149a4d6416123a2ce16a1c97de301562f03b
RedAvocado <minhcuong2922004@gmail.com>
2024-11-07 16:04:05 +0700

    chorus: small change
    Signed-off-by: RedAvocado <minhcuong2922004@gmail.com>

--- Ebay_Clone/src/java/controller/user/AccountController.java
+++ Ebay_Clone/src/java/controller/user/AccountController.java
@@ -30,15 +30,6 @@ import models.Product;
 @WebServlet(name = "AccountController", urlPatterns = {"/account"})
 public class AccountController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
